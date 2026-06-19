cd3ba50194c6ed81523e9278cbb90c2c723233f7
JAMAIN Simon <simon.jamain@etu.u-pec.fr>
2026-06-19 11:55:21 +0200

    Modification des importations de app.py dans le service 1

--- flask-services-kanban/service1_matrice/app.py
+++ flask-services-kanban/service1_matrice/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)                 
 
 def parse_matrix(data, key):
     """Convertit une liste de listes en tableau NumPy."""
